2f8d2f86cb783ca7446555c2eee766d84b3daad3
Emaan Tariq <emaantariq550@gmail.com>
2026-05-17 16:57:36 +0500

    fix cors for vercel frontend

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: '*',
+  origin: [
+    'http://localhost:3000',
+    'https://thriving-cobbler-1e879e.netlify.app',
+    'https://acadex-frontend-md44.vercel.app',
+    process.env.FRONTEND_URL
+  ],
   credentials: true
 }));
 app.use(express.json());
